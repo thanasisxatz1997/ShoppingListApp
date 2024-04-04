d5219a6e81b3256cbacd81b2de9d47cd0dc2fd68
achatziath <thanasisxatz1997@hotmail.com>
2024-04-04 11:58:00 +0300

    ShoppingList file

--- app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglistapp/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.shoppinglistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +30,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    ShoppingListApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ShoppingListAppTheme {
-        Greeting("Android")
     }
 }
